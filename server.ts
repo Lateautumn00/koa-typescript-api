98de1488aa9ea163fe9e7ed6b66d71c22950b69d
cuilanchao <cuilanchao@sina.com>
2021-07-16 19:35:39 +0800

    upd:

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import * as Koa from 'koa';
+import * as path from 'path';
 import { useControllers } from 'koa-controllers';
 const app = new Koa();
-useControllers(app, __dirname + '/app/controllers/*.controller.js', {
+useControllers(app, path.join(__dirname, '/app/controllers/*.controller.js'), {
   multipart: {
     dest: '',
   },
