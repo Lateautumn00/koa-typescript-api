2aebab0f3cbb36420e69f1f62981f8ced96ed38a
cuilanchao <cuilanchao@sina.com>
2021-07-20 15:01:58 +0800

    upd:改为箭头函数

--- app/modules/log4js.ts
+++ app/modules/log4js.ts
@@ -5,24 +5,24 @@ const resLogger: any = log4js.getLogger('resLogger');
 const errorLogger: any = log4js.getLogger('errorLogger');
 const consoleLogger: any = log4js.getLogger();
 const log4JsModules: any = {
-  logError: function (ctx, error, resTime) {
+  logError: (ctx: any, error: any, resTime: number) => {
     if (ctx && error) {
       errorLogger.error(formatError(ctx, error, resTime));
     }
   },
-  logResponse: function (ctx, resTime) {
+  logResponse: (ctx: any, resTime: number) => {
     if (ctx) {
       resLogger.info(formatRes(ctx, resTime));
     }
   },
-  logInfo: function (info) {
+  logInfo: (info: any) => {
     if (info) {
       consoleLogger.info(formatInfo(info));
     }
   },
 };
 
-const formatInfo = function (info) {
+const formatInfo = (info: any) => {
   return `***************info log start ***************
           info detail: ${JSON.stringify(info)}
           *************** info log end ****************
@@ -34,7 +34,7 @@ const formatInfo = function (info) {
  * @param resTime
  * @returns
  */
-const formatRes = function (ctx, resTime) {
+const formatRes = (ctx: any, resTime: number) => {
   return `*************** response log start ***************
           ${formatReqLog(ctx.request, resTime)}
           response status: ${ctx.status}
@@ -49,7 +49,7 @@ const formatRes = function (ctx, resTime) {
  * @param resTime
  * @returns
  */
-const formatError = function (ctx, err, resTime) {
+const formatError = (ctx: any, err: any, resTime: number) => {
   return `*************** error log start ***************
           ${formatReqLog(ctx.request, resTime)}
           err name: ${err.name}
@@ -64,7 +64,7 @@ const formatError = function (ctx, err, resTime) {
  * @param resTime
  * @returns
  */
-const formatReqLog = function (req, resTime) {
+const formatReqLog = (req: any, resTime: number) => {
   const method: string = req.method;
   const request: string =
     method === 'GET'
