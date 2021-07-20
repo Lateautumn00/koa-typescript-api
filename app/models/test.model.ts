18867bf79f3307418f7b62e2c3ee02c34d95ed02
cuilanchao <cuilanchao@sina.com>
2021-07-20 11:03:48 +0800

    upd:类

--- app/models/test.model.ts
+++ app/models/test.model.ts
@@ -1,14 +1,14 @@
 class TestModel {
-  text: any;
+  text: string;
   public async index(text: string) {
     const test1 = await this.getText1.call({ text: text });
     const test2 = await this.getText2(text);
     return `${test1} | ${test2}`;
   }
-  public async getText1() {
+  protected getText1() {
     return this.text;
   }
-  public async getText2(text: string) {
+  protected getText2(text: string) {
     return text;
   }
 }
