ca11448a53574144def7dac45ada0d79a959c385
cuilanchao <cuilanchao@sina.com>
2021-08-06 10:49:03 +0800

    upd

--- app/models/user.model.ts
+++ app/models/user.model.ts
@@ -2,7 +2,7 @@
  * @Description:usermodel
  * @Author: Lanchao cui
  * @Date: 2021-07-30 20:01:02
- * @LastEditTime: 2021-08-06 10:35:02
+ * @LastEditTime: 2021-08-06 10:48:02
  * @LastEditors: Lanchao cui
  * @Reference:
  */
@@ -19,7 +19,15 @@ class UserModel extends PersonModel {
   constructor() {
     super();
   }
-  async model(fun: string, where: object, data: object) {
+  /**
+   * @description:
+   * @param  {*}
+   * @return {*}
+   * @param {string} fun 方法名
+   * @param {object} where 条件
+   * @param {object} data 数据
+   */
+  async model(fun: string, where: object, data: object): Promise<object> {
     let returnData: object = {};
     switch (fun) {
       case 'create':
@@ -35,6 +43,7 @@ class UserModel extends PersonModel {
         returnData = await this.remove(where);
         break;
       default:
+        console.error('请求的mongodb方法还未在代码中实现');
         break;
     }
     return returnData;
@@ -47,27 +56,27 @@ class UserModel extends PersonModel {
     /**
      * 插入数据
      */
-    const addData: object = await createModel.save();
-    return addData;
+    const returnData: object = await createModel.save();
+    return returnData;
   }
   private async updateOne(where: object, data: object): Promise<object> {
-    const updateData = await userSchemaModel.updateOne(where, data);
+    const returnData = await userSchemaModel.updateOne(where, data);
     return {
-      status: updateData.n ? true : false,
+      status: returnData.n ? true : false,
     };
   }
   private async find(where: object): Promise<object> {
-    const list: Array<any> = await userSchemaModel.find(where);
+    const returnData: Array<any> = await userSchemaModel.find(where);
     return {
-      count: list.length,
-      data: list,
+      count: returnData.length,
+      data: returnData,
     };
   }
   private async remove(where: object): Promise<object> {
-    const removeData = await userSchemaModel.remove(where);
+    const returnData = await userSchemaModel.remove(where);
     return {
-      status: removeData.n ? true : false,
-      deletedCount: removeData.deletedCount,
+      status: returnData.n ? true : false,
+      deletedCount: returnData.deletedCount,
     };
   }
 }
