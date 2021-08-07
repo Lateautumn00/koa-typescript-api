ea2235c007384730e11d8456b499efca969a1f49
cuilanchao <cuilanchao@sina.com>
2021-08-07 11:09:58 +0800

    upd:删除@types引用，替换为.d.ts文件声明

---
+++ typings/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @description: 找不到的第三方包
+ * @param  {*}
+ * @return {*}
+ */
+declare module 'koa';
+declare module 'koa2-cors';
+declare module 'koa-router';
+declare module 'koa-bodyparser';
+declare module 'ws';
